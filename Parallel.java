21549fc93d018d3b32de09b4a52eb12a875b0328
shriyahardikar <shriyasu@buffalo.edu>
2018-05-01 11:01:01 -0400

    Removed test class

--- Parallel.java
+++ Parallel.java
@@ -1,9 +1,50 @@
-public class Parallel{
-
-    private static int temp;
+class Parallel implements Runnable {
+    private Thread t;
+    private String threadName;
+    
+    Parallel( String name) {
+       threadName = name;
+       System.out.println("Creating " +  threadName );
+    }
+    
+    public void run() {
+       System.out.println("Running " +  threadName );
+       try {
+          for(int i = 4; i > 0; i--) {
+             System.out.println("Thread: " + threadName + ", " + i);
+             // Let the thread sleep for a while.
+             Thread.sleep(50);
+          }
+       } catch (InterruptedException e) {
+          System.out.println("Thread " +  threadName + " interrupted.");
+       }
+       System.out.println("Thread " +  threadName + " exiting.");
+    }
+    
+    public void start () {
+       System.out.println("Starting " +  threadName );
+       if (t == null) {
+          t = new Thread (this, threadName);
+          t.start ();
+       }
+    }
 
     public static void main(String[] args){
+        Parallel thread = new Parallel("Thread 1");
+        thread.start();
 
-        System.out.println("HEllo");
+        Parallel thread2 = new Parallel("Thread 2");
+        thread2.start();
     }
-}
+ }
+ 
+//  public class TestThread {
+ 
+//     public static void main(String args[]) {
+//        RunnableDemo R1 = new RunnableDemo( "Thread-1");
+//        R1.start();
+       
+//        RunnableDemo R2 = new RunnableDemo( "Thread-2");
+//        R2.start();
+//     }   
+//  }
