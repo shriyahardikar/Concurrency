adba0ed82e465ee451b48ec98a2bc54cd7d7102b
shriyahardikar <shriyasu@buffalo.edu>
2018-05-13 19:44:04 -0400

    Added abstraction
    Added abstraction

--- Parallel.java
+++ Parallel.java
@@ -1,50 +1,52 @@
-class Parallel implements Runnable {
+class MyThread implements Runnable {
     private Thread t;
-    private String threadName;
+    private String taskName;
     
-    Parallel( String name) {
-       threadName = name;
-       System.out.println("Creating " +  threadName );
+    MyThread(String taskName) {
+        this.taskName = taskName;
+        System.out.println("Create task " + taskName);
     }
     
     public void run() {
-       System.out.println("Running " +  threadName );
-       try {
-          for(int i = 4; i > 0; i--) {
-             System.out.println("Thread: " + threadName + ", " + i);
-             // Let the thread sleep for a while.
-             Thread.sleep(50);
-          }
-       } catch (InterruptedException e) {
-          System.out.println("Thread " +  threadName + " interrupted.");
-       }
-       System.out.println("Thread " +  threadName + " exiting.");
+        System.out.println("Running task " + taskName);
+        try{
+            for(int i = 4; i > 0; i--) {
+                System.out.println("Task: " + taskName + ", " + i);
+                Thread.sleep(50);
+            }
+        }
+        catch(InterruptedException e){
+            System.out.println("Task " + taskName + " interrupted.");
+        }
+        System.out.println("Task " + taskName + " exiting.");
     }
     
-    public void start () {
-       System.out.println("Starting " +  threadName );
-       if (t == null) {
-          t = new Thread (this, threadName);
-          t.start ();
-       }
+    public void start(){
+        System.out.println("Starting task " + taskName );
+        if(t == null) {
+            t = new Thread (this, taskName);
+            t.start ();
+        }
     }
 
+    // public static void main(String[] args){
+    //     Parallel task1 = new Parallel("Create list");
+    //     task1.start();
+
+    //     Parallel task2 = new Parallel("Deploy list");
+    //     task2.start();
+    // }
+}
+
+//Executable class: user interaction 
+class Parallel{
     public static void main(String[] args){
-        Parallel thread = new Parallel("Thread 1");
-        thread.start();
+        MyThread task1 = new MyThread("Create list");
+        task1.start();
 
-        Parallel thread2 = new Parallel("Thread 2");
-        thread2.start();
+        MyThread task2 = new MyThread("Deploy list");
+        task2.start();
     }
- }
+}
+
  
-//  public class TestThread {
- 
-//     public static void main(String args[]) {
-//        RunnableDemo R1 = new RunnableDemo( "Thread-1");
-//        R1.start();
-       
-//        RunnableDemo R2 = new RunnableDemo( "Thread-2");
-//        R2.start();
-//     }   
-//  }
